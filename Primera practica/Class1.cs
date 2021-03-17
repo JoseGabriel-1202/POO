4af737b66f07c9f35cb5eb8f819a52669fa517a9
JoseGabriel-1202 <78770273+JoseGabriel-1202@users.noreply.github.com>
2021-03-17 17:35:41 -0300

    Add files via upload

---
+++ Primera practica/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POO_primera_practica
+{
+    class Persona
+    {
+        String Nombre = "Jose", Apellido = "Campusano"; 
+        int edad=20;
+        long Cedula = 402-3378055-6;
+
+        public void Responsabilidad()
+        {
+            Console.WriteLine("---Hola---");
+
+            Console.Write(" Mi nombre es: " + Nombre+
+                "\n Mi apellido es: " + Apellido +
+                "\n Mi edad es: " + edad+
+                "\n Mi cedula es: " + Cedula);
+        }
+
+    }
+    class Profesor : Persona
+    {
+        int sueldo = 20000;
+        public void Sueldo()
+        {
+            Console.WriteLine("Mi sueldo es: " + sueldo);
+        }
+    }
+   
+}
